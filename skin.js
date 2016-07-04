05b022ad669f5b86ac2665abae1e78f9a30b2273
Jason Li <jason.li@aspectsecurity.com>
2016-07-04 00:59:46 -0400

    adding customization script

---
+++ skin.js
@@ -0,0 +1,18 @@
+javascript:(function(){
+
+	document.querySelector( ".banner" ).style.backgroundColor="#DE1F26";
+	document.querySelector( ".banner" ).style.backgroundImage="none";
+
+	document.querySelectorAll( ".banner h1" ).forEach(function(n) {
+		n.style.color="#FCF593";
+	});
+
+	// remove Instagram
+	
+	// adjust join text
+
+	// adjust brought to you by
+
+	// adjust logo
+})();
+
